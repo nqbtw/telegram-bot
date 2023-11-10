438aaa85f415f01221111fd25d346b7176058410
Алескей Алиев - nqbtw <vvkpnq2077@gmail.com>
2023-11-10 17:26:59 +0300

    ВЗАИМОДЕЙСТВИЕ

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import os
+from dotenv import load_dotenv    # pip install python-dotenv
 import requests
+# pip install python-telegram-bot==13.7
+from telegram import Bot
+from telegram.ext import Updater, MessageHandler, Filters
+
+load_dotenv()
+ТОКЕН = os.getenv('TOKEN')
+БОТ = Bot(ТОКЕН)
+
+def отправить_сообщение(чат, сообщение):
+    БОТ.send_message(chat_id=чат, text=сообщение)
 
 def запарсить_погоду(город):
 
@@ -11,8 +23,25 @@ def запарсить_погоду(город):
     старт = текст.find('>', старт - 10)
     стоп = текст.find('<', старт)
     ответ = текст[старт + 1:стоп]
+    if len (ответ) == 0:
+        return "Город не распознан"
+    return ответ
 
-    print(ответ)
+def реакция(информация, контекст):
+    print('реакция')
 
-запарсить_погоду('Казань')
+def запустить_бота():
+    обновление = Updater(ТОКЕН)
+    # Эта строка позволяет реагировать на входящие текстовые сообщения
+    обновление.dispatcher.add_handler(MessageHandler(Filters.text, реакция))
+    # Эта строка заставляет код опрашивать сервер телеграмма с заданным интервалом
+    обновление.start_polling(poll_interval=1)
+    print('бот запущен')
+    # запускает процесс опроса сервера (выполняется бесконечно)
+    обновление.idle()
 
+
+print(запарсить_погоду(''))
+
+отправить_сообщение(923992817, "ВСЫВЫВС")
+запустить_бота()
