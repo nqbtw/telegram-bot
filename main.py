d361c75fe555198ca80468f405efcf0ed670f92f
Алескей Алиев - nqbtw <vvkpnq2077@gmail.com>
2023-11-17 17:45:39 +0300

    код отправляем на удалёнку

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv    # pip install python-dotenv
 import requests
 # pip install python-telegram-bot==13.7
-from telegram import Bot
+from telegram import Bot, ReplyKeyboardMarkup
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 
 load_dotenv()
@@ -10,7 +10,11 @@ load_dotenv()
 БОТ = Bot(ТОКЕН)
 
 def отправить_сообщение(чат, сообщение):
-    БОТ.send_message(chat_id=чат, text=сообщение)
+    кнопки = [["Коломна", "Воскренск", "Москва"], ["/help"]]
+    рег_кнопки = ReplyKeyboardMarkup(кнопки, resize_keyboard=True)
+    БОТ.send_message(chat_id=чат.id, text=сообщение, reply_markup=рег_кнопки)
+    имя_пользователя = чат.first_name
+    
 
 def запарсить_погоду(город):
 
@@ -29,25 +33,35 @@ def запарсить_погоду(город):
 
 def реакция(информация, контекст):
     текст_сообщение = информация.effective_message.text
-    ид_чата = информация.effective_message.chat.id
+    ид_чата = информация.effective_message.chat
     погода = запарсить_погоду(текст_сообщение)
     print(f'Запрос - "{текст_сообщение}". Ответ - "{погода}"')
     отправить_сообщение(ид_чата, погода)
 
 def старт(информация, контекст):
-    ид_чата = информация.effective_message.chat.id
+    ид_чата = информация.effective_message.chat
     имя_пользователя = информация._effective_message.chat.first_name
     сообщение = f"""Привет, {имя_пользователя}!
 
 Бот поможет тебе узнать погоду в нужном для тебя городе. Введи название интересующего тебя города для получении информации"""
     отправить_сообщение(ид_чата, сообщение)
 
+def help(информация, контекст):
+    ид_чата = информация.effective_message.chat
+    имя_пользователя = информация._effective_message.chat.first_name
+    сообщение = f"""Привет, {имя_пользователя}!
+
+Бот поможет тебе узнать погоду в нужном для тебя городе. Введи название интересующего тебя города для получении информации"""
+    отправить_сообщение(ид_чата, сообщение)
+
+
 def запустить_бота():
     обновление = Updater(ТОКЕН)
 
     # эта строка позволяет реагировать на стандартные команды, которые пользователь пишет через /, например /start 
     обновление.dispatcher.add_handler(CommandHandler('start', старт))
-    
+    обновление.dispatcher.add_handler(CommandHandler('help', help))
+
     # Эта строка позволяет реагировать на входящие текстовые сообщения
     обновление.dispatcher.add_handler(MessageHandler(Filters.text, реакция))
 
